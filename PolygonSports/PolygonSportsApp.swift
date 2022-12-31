35c095c134a15836f71cf14eb5546d544b53c2c3
Anish Rangdal <anish.rangdal@icloud.com>
2022-12-31 17:09:42 -0500

    Initial Commit

---
+++ PolygonSports/PolygonSportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolygonSportsApp.swift
+//  PolygonSports
+//
+//  Created by Anish Rangdal on 12/31/22.
+//
+
+import SwiftUI
+
+@main
+struct PolygonSportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
